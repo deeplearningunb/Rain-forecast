0f20481651615f6e5efbe3dbd36f4430e0df12b6
Arthur Bringel <arthurbringel@gmail.com>
2021-05-21 18:49:50 -0300

    Clean columns from the csv file

--- preprocessing.py
+++ preprocessing.py
@@ -1,11 +1,25 @@
 def derive_nth_day_feature(df, feature, N):
     rows = df.shape[0]
-    nth_prior_measurements = [None]*N + [df[feature][i-N] for i in range(N, rows)]
+    nth_prior_measurements = [None] * N + [df[feature][i - N] for i in range(N, rows)]
     col_name = "{}_{}".format(feature, N)
     df[col_name] = nth_prior_measurements
 
 
 def dataframe_sort(df):
+    # make list of original features without meantempm, mintempm, and maxtempm
+    to_remove = [feature
+                 for feature in df
+                 if feature not in ['Precipitacao', 'TempBulboSeco', 'TempBulboUmido', 'TempMaxima', 'TempMinima',
+                                    'UmidadeRelativa', 'PressaoAtmEstacao', 'PressaoAtmMar', 'DirecaoVento',
+                                    'VelocidadeVento', 'Insolacao', 'Nebulosidade', 'Evaporacao Piche',
+                                    'Temp Comp Media', 'Umidade Relativa Media', 'Velocidade do Vento Media']]
+
+    # make a list of columns to keep
+    to_keep = [col for col in df.columns if col not in to_remove]
+
+    # select only the columns in to_keep and assign to df
+    df = df[to_keep]
+
     for feature in df:
         if feature != 'Data':
             for N in range(1, 4):
